3ea6962724288c0c5a4d64c6e084c882c3f3c158
crja73 <57744842+crja73@users.noreply.github.com>
2021-09-09 23:04:36 +0300

    Add files via upload
    добавлен адекватное распознавание языка + ответка

--- accederz_support.py
+++ accederz_support.py
@@ -1,26 +1,53 @@
 import telebot
 from telebot import types
 
+scenario = {}
+
 bot = telebot.TeleBot('1937575560:AAEfovYaNsDEWo3xRocUE4uX3Y9COc9broY')
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(content_types=['text'])  # comands=
+
 def start_command(message):
-	if message != 'admin':
-	    print('said hello')
-	    keyboard = types.InlineKeyboardMarkup()
-	    russian = types.InlineKeyboardButton(text='RU', callback_data='ru')
-	    keyboard.add(russian)
-	    english = types.InlineKeyboardButton(text='EN', callback_data='en')
-	    keyboard.add(english)
-	    bot.send_message(message.from_user.id, text='Выбери язык / Choose your language ', reply_markup=keyboard)
+	if message.text != 'admin':
+		if str(message.text)[0].lower() in 'йцукенгшщзхъфывапролджэячсмитьбю':
+			print('said hello')
+			keyboard = types.InlineKeyboardMarkup()
+			russian = types.InlineKeyboardButton(text='Проблема со входом', callback_data='enter')
+			keyboard.add(russian)
+
+			english = types.InlineKeyboardButton(text='Не работает чит', callback_data='work')
+			keyboard.add(english)
+
+			bot.send_message(message.from_user.id, text='Выберите один из вариантов', reply_markup=keyboard)
+
+		else:
+			keyboard = types.InlineKeyboardMarkup()
+			russian = types.InlineKeyboardButton(text='Login problem', callback_data='enter_en')
+			keyboard.add(russian)
+
+			english = types.InlineKeyboardButton(text='Cheat does not work', callback_data='work_en')
+			keyboard.add(english)
+
+			bot.send_message(message.from_user.id, text='Choose one of the options', reply_markup=keyboard)
+
 	else:
-		pas
+		bot.send_message(message.from_user.id, text='Введите пароль')
+		if message.text == 'password':
+			print('Добро пожаловать')
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-	if call.data == "ru":
-		bot.send_message(call.message.chat.id, "Привет!")
-	else:
-		bot.send_message(call.message.chat.id, "Hello!")
+	if call.data == "enter":
+		bot.send_message(call.message.chat.id, "Введи правильный пароль, долбоеб")
+
+	elif call.data == 'work':
+		bot.send_message(call.message.chat.id, "Потому что ты чмо")
+
+	elif call.data == 'enter_en':
+		bot.send_message(call.message.chat.id, "Enter the correct password, dumbass")
+
+	elif call.data == 'work_en':
+		bot.send_message(call.message.chat.id, "Because you are a schmuck")
+
 bot.polling()
